9944a38acdc3175462eef955e2ff0ad76e11902f
Jackson <jrgray609@gmail.com>
2026-02-19 21:16:41 -0700

    tdd

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -9,6 +9,7 @@ test('updateUser', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Password' }).fill('diner');
     await page.getByRole('button', { name: 'Register' }).click();
 
+     await expect(page.getByRole('link', { name: 'pd' })).toBeVisible();
     await page.getByRole('link', { name: 'pd' }).click();
 
     await expect(page.getByRole('main')).toContainText('pizza diner');
