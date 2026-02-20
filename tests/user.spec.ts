e3a81ab38524b75acc99198e5fbaad6f4b648e10
Jackson <jrgray609@gmail.com>
2026-02-19 21:39:57 -0700

    tdd

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -2,13 +2,14 @@ import { test, expect } from 'playwright-test-coverage';
 
 test('updateUser', async ({ page }) => {
     const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
-    await page.goto('/');
+    await page.goto('http://localhost:5173/');
     await page.getByRole('link', { name: 'Register' }).click();
     await page.getByRole('textbox', { name: 'Full name' }).fill('pizza diner');
     await page.getByRole('textbox', { name: 'Email address' }).fill(email);
     await page.getByRole('textbox', { name: 'Password' }).fill('diner');
     await page.getByRole('button', { name: 'Register' }).click();
 
+    await expect(page.getByRole('link', { name: 'pd' })).toBeVisible()
     await page.getByRole('link', { name: 'pd' }).click();
 
     await expect(page.getByRole('main')).toContainText('pizza diner');
